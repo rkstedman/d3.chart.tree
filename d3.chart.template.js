43e9f8ec24e26c6d74c014d6b86dd8d963eaf9f8
Rachael Stedman <rkstedman@gmail.com>
2014-03-01 14:20:41 -0500

    initial commit with d3.chart.template

---
+++ d3.chart.template.js
@@ -0,0 +1,12 @@
+/*! d3.chart.template - v0.1.0
+ *  License: MIT Expat
+ *  Date: 2013-10-16
+ */
+d3.chart("TemplateChart", {
+  initialize: function() {
+
+  }
+});
+d3.chart("TemplateChart").extend("ExtendedTemplateChart", {
+
+});
